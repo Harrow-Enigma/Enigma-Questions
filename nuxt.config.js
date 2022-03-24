de3593a7f50f1662b106581c909869d4a79cb391
Dylan K <50585244+dylankainth@users.noreply.github.com>
2022-03-24 09:47:50 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "The Enigma Problems",
+    title: "The Enigma Questions",
     htmlAttrs: {
       lang: "en",
     },
